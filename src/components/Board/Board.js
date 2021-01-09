3c9638e708749b694885b3bf5675f910cd8da06a
Ashish Patel <apatel.developer@gmail.com>
2021-01-08 20:03:01 -0500

    board complete

---
+++ src/components/Board/Board.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import Piece from '../Piece/Piece.js'
+import './Board.css'
+
+
+class Board extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state = {chosenPiece : '', prevSquare:''};
+      this.referencesMap = new Map();
+    }
+
+    renderSquare(loc) {
+      let sqrRef = React.createRef();
+      return( 
+        <div className='Square' onClick= {() => {
+          
+          if(this.state.chosenPiece != ''){
+            sqrRef.current.setState({piece:this.state.chosenPiece})
+            this.setState({chosenPiece:''});
+            console.log(this.referencesMap);
+          }else{
+            this.setState({chosenPiece:sqrRef.current.state.piece});
+            this.setState({prevSquare: loc})
+            sqrRef.current.setState({piece:''})
+          }
+          
+        }}>
+          <Piece value={loc} ref={sqrRef}/>
+        </div>
+        )
+    }
+
+    render() {
+      return (
+        <div className='Board'>
+          <div className='board-row'>
+            {this.renderSquare('A8')}
+            {this.renderSquare('B8')}
+            {this.renderSquare('C8')}
+            {this.renderSquare('D8')}
+            {this.renderSquare('E8')}
+            {this.renderSquare('F8')}
+            {this.renderSquare('G8')}
+            {this.renderSquare('H8')}
+          </div>
+
+          <div className='board-row'>
+            {this.renderSquare('A7')}
+            {this.renderSquare('B7')}
+            {this.renderSquare('C7')}
+            {this.renderSquare('D7')}
+            {this.renderSquare('E7')}
+            {this.renderSquare('F7')}
+            {this.renderSquare('G7')}
+            {this.renderSquare('H7')}
+          </div>
+
+          <div className='board-row'>
+            {this.renderSquare('A6')}
+            {this.renderSquare('B6')}
+            {this.renderSquare('C6')}
+            {this.renderSquare('D6')}
+            {this.renderSquare('E6')}
+            {this.renderSquare('F6')}
+            {this.renderSquare('G6')}
+            {this.renderSquare('H6')}
+          </div>
+
+          <div className='board-row'>
+            {this.renderSquare('A5')}
+            {this.renderSquare('B5')}
+            {this.renderSquare('C5')}
+            {this.renderSquare('D5')}
+            {this.renderSquare('E5')}
+            {this.renderSquare('F5')}
+            {this.renderSquare('G5')}
+            {this.renderSquare('H5')}
+          </div>
+
+          <div className='board-row'>
+            {this.renderSquare('A4')}
+            {this.renderSquare('B4')}
+            {this.renderSquare('C4')}
+            {this.renderSquare('D4')}
+            {this.renderSquare('E4')}
+            {this.renderSquare('F4')}
+            {this.renderSquare('G4')}
+            {this.renderSquare('H4')}
+          </div>
+
+          <div className='board-row'>
+            {this.renderSquare('A3')}
+            {this.renderSquare('B3')}
+            {this.renderSquare('C3')}
+            {this.renderSquare('D3')}
+            {this.renderSquare('E3')}
+            {this.renderSquare('F3')}
+            {this.renderSquare('G3')}
+            {this.renderSquare('H3')}
+          </div>
+
+          <div className='board-row'>
+            {this.renderSquare('A2')}
+            {this.renderSquare('B2')}
+            {this.renderSquare('C2')}
+            {this.renderSquare('D2')}
+            {this.renderSquare('E2')}
+            {this.renderSquare('F2')}
+            {this.renderSquare('G2')}
+            {this.renderSquare('H2')}
+          </div>
+
+          <div className='board-row'>
+            {this.renderSquare('A1')}
+            {this.renderSquare('B1')}
+            {this.renderSquare('C1')}
+            {this.renderSquare('D1')}
+            {this.renderSquare('E1')}
+            {this.renderSquare('F1')}
+            {this.renderSquare('G1')}
+            {this.renderSquare('H1')}
+          </div>
+        </div>
+      );
+    }
+  }
+
+export default Board;
---
+++ src/components/Piece/Piece.js
@@ -0,0 +1,127 @@
+import React from 'react'
+import './Piece.css'
+
+//IMAGES
+import black_king from '../../images/black_king.svg'
+import black_queen from '../../images/black_queen.svg'
+import black_rook from '../../images/black_rook.svg'
+import black_bishop from '../../images/black_bishop.svg'
+import black_knight from '../../images/black_knight.svg'
+import black_pawn from '../../images/black_pawn.svg'
+
+import white_king from '../../images/white_king.svg'
+import white_queen from '../../images/white_queen.svg'
+import white_rook from '../../images/white_rook.svg'
+import white_bishop from '../../images/white_bishop.svg'
+import white_knight from '../../images/white_knight.svg'
+import white_pawn from '../../images/white_pawn.svg'
+
+
+
+class Piece extends React.Component{
+
+    constructor(props){
+        super(props)
+        
+        //pawns
+        if(this.props.value.includes('2')){
+            this.state = {piece: 'P'}; //white
+        }
+        else if(this.props.value.includes('7')){
+            this.state = {piece: 'p'}; //black
+        }
+
+        //rooks
+        else if(this.props.value.includes('1') && (this.props.value.includes('A') || this.props.value.includes('H'))){
+            this.state = {piece: 'R'}; //white
+        }
+        else if(this.props.value.includes('8') && (this.props.value.includes('A') || this.props.value.includes('H'))){
+            this.state = {piece: 'r'}; //black
+        }
+
+        //knight
+        else if(this.props.value.includes('1') && (this.props.value.includes('B') || this.props.value.includes('G'))){
+            this.state = {piece: 'N'}; //white
+        }
+        else if(this.props.value.includes('8') && (this.props.value.includes('B') || this.props.value.includes('G'))){
+            this.state = {piece: 'n'}; //black
+        }
+
+        //knight
+        else if(this.props.value.includes('1') && (this.props.value.includes('C') || this.props.value.includes('F'))){
+            this.state = {piece: 'B'}; //white
+        }
+        else if(this.props.value.includes('8') && (this.props.value.includes('C') || this.props.value.includes('F'))){
+            this.state = {piece: 'b'}; //black
+        }
+
+        //Queen
+        else if(this.props.value == 'D1'){
+            this.state = {piece: 'Q'}; //white
+        }
+        else if(this.props.value == 'D8'){
+            this.state = {piece: 'q'}; //white
+        }
+
+        //King
+        else if(this.props.value == 'E1'){
+            this.state = {piece: 'K'}; //white
+        }
+        else if(this.props.value == 'E8'){
+            this.state = {piece: 'k'}; //white
+        }
+
+
+
+        else{
+            this.state = {piece: ''};
+        }
+        
+    }
+
+    render(){
+      let img_src = "";
+      if(this.state.piece == 'P'){
+        img_src = white_pawn;
+      }
+      else if(this.state.piece == 'p'){
+        img_src = black_pawn;
+      }
+      else if(this.state.piece == 'B'){
+        img_src = white_bishop;
+      }
+      else if(this.state.piece == 'b'){
+        img_src = black_bishop;
+      }
+      else if(this.state.piece == 'N'){
+        img_src = white_knight;
+      }
+      else if(this.state.piece == 'n'){
+        img_src = black_knight;
+      }
+      else if(this.state.piece == 'R'){
+        img_src = white_rook;
+      }
+      else if(this.state.piece == 'r'){
+        img_src = black_rook;
+      }
+      else if(this.state.piece == 'K'){
+        img_src = white_king;
+      }
+      else if(this.state.piece == 'k'){
+        img_src = black_king;
+      }
+      else if(this.state.piece == 'Q'){
+        img_src = white_queen;
+      }
+      else if(this.state.piece == 'q'){
+        img_src = black_queen;
+      }
+
+      return(
+          <img src={img_src} alt={this.state.piece}/>
+      )
+    }
+}
+
+export default Piece;
